c3adf9665c1cc6328ff6ef0a6d9d82b9aaa10768
Hashib Raja <hashibraja23@gmail.com>
2023-10-13 08:17:37 +0530

    when menu is loading showing loading component 🔦🔦

--- src/ui/AppLayout.tsx
+++ src/ui/AppLayout.tsx
@@ -1,10 +1,14 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Header from "./Header";
 import CartOverview from "../features/cart/CartOverview";
+import Loader from "./Loader";
 
 const AppLayout = () => {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
   return (
-    <div>
+    <div className="layout">
+      {isLoading && <Loader />}
       <Header />
       <main>
         <Outlet />
