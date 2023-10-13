b52d373bfa58ba6b7ef08b24512399cc85caee50
Hashib Raja <hashibraja23@gmail.com>
2023-10-13 06:53:41 +0530

    Implemented routes using new syntax 🖋️🖋️

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/menu", element: <Menu /> },
+]);
+
 const App = () => {
-  return <div>App</div>;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
