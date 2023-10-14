e6f05c853a6a8ddc4a272c60d92a6899bcdc1727
Hashib Raja <hashibraja23@gmail.com>
2023-10-14 08:14:54 +0530

    given loader absolute class and showing loading 🩻🩻

--- src/ui/Loader.tsx
+++ src/ui/Loader.tsx
@@ -1,5 +1,9 @@
 const Loader = () => {
-  return <div className="loader">Loader</div>;
+  return (
+    <div className="absolute inset-0 flex items-center justify-center bg-stone-200/50 backdrop-blur-sm">
+      <div className="loader"></div>
+    </div>
+  );
 };
 
 export default Loader;
