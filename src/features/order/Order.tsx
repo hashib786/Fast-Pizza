be5c05370f7258bd98224022d3a9a254c13b9be5
Hashib Raja <hashibraja23@gmail.com>
2023-10-15 07:24:13 +0530

    Added tailwind classes in order detail page 🍕🍕

--- src/features/order/Order.tsx
+++ src/features/order/Order.tsx
@@ -1,11 +1,13 @@
 // Test ID: IIDSAT
 
-import { useLoaderData } from "react-router-dom";
+import { LoaderFunctionArgs, useLoaderData } from "react-router-dom";
+import { getOrder } from "../../services/apiRestaurant";
 import {
   calcMinutesLeft,
   formatCurrency,
   formatDate,
 } from "../../utils/helpers";
+import OrderItem from "./OrderItem";
 
 export interface CartI {
   pizzaId?: number;
@@ -31,40 +33,67 @@ export interface OrderI {
 
 function Order() {
   const order = useLoaderData() as OrderI;
-  console.log(order);
 
   // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
-  const { status, priority, priorityPrice, orderPrice, estimatedDelivery } =
-    order;
+  const {
+    cart,
+    id,
+    status,
+    priority,
+    priorityPrice,
+    orderPrice,
+    estimatedDelivery,
+  } = order;
   const deliveryIn = calcMinutesLeft(estimatedDelivery);
 
   return (
-    <div>
-      <div>
-        <h2>Status</h2>
+    <div className="px-4 py-6">
+      <div className="flex flex-wrap items-center justify-between gap-2">
+        <h2 className="text-base font-bold ">Order #{id} Status</h2>
 
-        <div>
-          {priority && <span>Priority</span>}
-          <span>{status} order</span>
+        <div className="space-x-2">
+          {priority && (
+            <span className="rounded-full bg-red-600 px-2 py-1 text-xs text-red-100">
+              Priority
+            </span>
+          )}
+          <span className="rounded-full bg-green-600 px-2 py-1 text-xs capitalize text-red-100">
+            {status} order
+          </span>
         </div>
       </div>
 
-      <div>
-        <p>
+      <div className="my-8 flex flex-wrap items-center justify-between gap-2 bg-stone-300 px-3 py-6">
+        <p className="font-bold ">
           {deliveryIn >= 0
             ? `Only ${calcMinutesLeft(estimatedDelivery)} minutes left 😃`
             : "Order should have arrived"}
         </p>
-        <p>(Estimated delivery: {formatDate(estimatedDelivery)})</p>
+        <p className="text-sm">
+          (Estimated delivery: {formatDate(estimatedDelivery)})
+        </p>
       </div>
 
-      <div>
+      <ul className="list-none divide-y divide-stone-300 border-y border-stone-300">
+        {cart.map((item) => (
+          <OrderItem key={item.pizzaId} item={item} />
+        ))}
+      </ul>
+
+      <div className="my-8 gap-2 space-y-2 bg-stone-300 px-3 py-6 text-sm">
         <p>Price pizza: {formatCurrency(orderPrice)}</p>
         {priority && <p>Price priority: {formatCurrency(priorityPrice)}</p>}
-        <p>To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}</p>
+        <p className="font-bold">
+          To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}
+        </p>
       </div>
     </div>
   );
 }
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const orderLoader = async ({ params }: LoaderFunctionArgs) => {
+  return await getOrder(params.orderId || "Wrong");
+};
 
 export default Order;
--- src/features/order/OrderItem.tsx
+++ src/features/order/OrderItem.tsx
@@ -1,16 +1,19 @@
-// function OrderItem({ item, isLoadingIngredients, ingredients }) {
-//   const { quantity, name, totalPrice } = item;
+import { formatCurrency } from "../../utils/helpers";
+import { CartI } from "./Order";
 
-//   return (
-//     <li>
-//       <div>
-//         <p>
-//           <span>{quantity}&times;</span> {name}
-//         </p>
-//         <p>{formatCurrency(totalPrice)}</p>
-//       </div>
-//     </li>
-//   );
-// }
+function OrderItem({ item }: { item: CartI }) {
+  const { quantity, name, totalPrice } = item;
 
-// export default OrderItem;
+  return (
+    <li>
+      <div className="flex flex-wrap items-center justify-between gap-2 px-2 py-3">
+        <p>
+          <span>{quantity}&times;</span> {name}
+        </p>
+        <p className="font-bold">{formatCurrency(totalPrice)}</p>
+      </div>
+    </li>
+  );
+}
+
+export default OrderItem;
